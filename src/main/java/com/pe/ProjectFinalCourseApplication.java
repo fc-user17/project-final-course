82254777f38e175e3060dedf6f21acb5192d8383
fc-user17 <gibbfc30@gmail.com>
2026-09-05 08:15:25 -0500

    add project

---
+++ src/main/java/com/pe/ProjectFinalCourseApplication.java
@@ -0,0 +1,13 @@
+package com.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFinalCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectFinalCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pe/ProjectFinalCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFinalCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
